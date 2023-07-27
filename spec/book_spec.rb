341e67ef9645812cdf052ef1b03eee592612afcb
Antoine Aube <aaube3@gmail.com>
2023-07-26 18:02:07 -0600

    test: initialize book with name, author and publication year

---
+++ spec/book_spec.rb
@@ -0,0 +1,22 @@
+require './lib/book'
+
+RSpec.decscribe Book do 
+  describe "iteration 1" do 
+    it "exists, has name, author, and publication year" do 
+      book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+      #=> #<Book:0x00007fcc021fad08...>
+      
+      expect(book).to be_a Book
+      expect(book.title).to eq("To Kill a Mockingbird")
+      #=> "To Kill a Mockingbird"
+      
+      expect(book.author).to eq("Harper Lee")
+      #=> "Harper Lee"
+      
+      expect(book.publication_year).to eq("1960")
+    end 
+  end 
+end
+
+
+#=> "1960"
