86cde5ac8e7da2861b0df9797e1fae7407bdf7eb
Antoine Aube <aaube3@gmail.com>
2023-07-26 19:34:29 -0600

    test: #publication_time_frame_for

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -46,8 +46,18 @@ RSpec.describe Library do
       dpl = Library.new("Denver Public Library")
       dpl.add_author(@charlotte_bronte)
       dpl.add_author(@harper_lee)
-
+      
       expect(dpl.books).to eq([@jane_eyre, @professor, @villette, @mockingbird])
+    end
+  end
+  
+  describe "publication_time_frame_for" do 
+    it "returns a publication timeframe for a certain author" do 
+      dpl = Library.new("Denver Public Library")
+      dpl.add_author(@charlotte_bronte)
+      dpl.add_author(@harper_lee)
+      # require 'pry';binding.pry
+      expect(dpl.publication_time_frame_for(@charlotte_bronte)).to eq({:start => "1847", :end => "1857"})
     end
   end
 end
