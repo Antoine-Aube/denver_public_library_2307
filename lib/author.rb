9669e5773ffebe5ec660e5e044af3da4289be2b9
Antoine Aube <aaube3@gmail.com>
2023-07-26 18:40:04 -0600

    #write for Author class

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,20 @@
 class Author
   attr_reader :name, :books
   def initialize(author_info)
+    @first_name = author_info[:first_name]
+    @last_name = author_info[:last_name]
     @name = "#{author_info[:first_name]} #{author_info[:last_name]}"
     @books = []
+  end
+
+  def write(title, publication_date)
+    new_book = Book.new({
+      title: title,
+      author_first_name: @first_name,
+      author_last_name: @last_name,
+      publication_date: publication_date
+    })
+    @books << new_book
+    new_book
   end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -17,8 +17,27 @@ RSpec.describe Author do
     
     it "has an empty array of books" do 
       charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-
+      
       expect(charlotte_bronte.books).to eq([])
     end
   end
+  
+  describe "#write" do 
+    it "can create book objects" do 
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      
+      expect(jane_eyre.class).to eq(Book)
+      expect(jane_eyre.title).to eq("Jane Eyre")
+    end 
+
+  
+    it "can add books to its books array with the write method" do 
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      villette = charlotte_bronte.write("Villette", "1853")
+
+      expect(charlotte_bronte.books).to eq([jane_eyre, villette])
+    end
+  end 
 end
